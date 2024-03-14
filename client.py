0ff332f34fe6afd78bf27a8bb146c33733aca52a
Ed Packard <edward.packard@kinandcarta.com>
2024-03-14 11:05:37 +0000

    send an avro message

--- client.py
+++ client.py
@@ -1,12 +1,26 @@
+import io
 import socket
+import avro.datafile
+import avro.schema
+import avro.io
+import avro.ipc
 
-def send_message(connection):
-    connection.send("hello world".encode('utf-8'))
+SCHEMA = avro.schema.parse(open("./user.avsc").read())
+
+def send_message(connection, message):
+    buf = io.BytesIO()
+    writer = avro.datafile.DataFileWriter(buf, 
+        avro.io.DatumWriter(), SCHEMA)
+    writer.append(message)
+    writer.flush()
+    buf.seek(0)
+    data= buf.read()
+    connection.send(data)
 
 def main(): 
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    connection.connect(('127.0.0.1', 12345))
-    send_message(connection)
+    connection.connect(("127.0.0.1", 12345))
+    send_message(connection, {"name": "Ben", "favorite_number": 7, "favorite_color": "red"})
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -2,12 +2,12 @@ import socket
 
 def handle_client (connection, address):
     data = connection.recv(1024)
-    print("Received message:", data.decode('utf-8'))
+    print("Received message:", data)
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(('127.0.0.1', 12345))
+    sock.bind(("127.0.0.1", 12345))
     sock.listen(10)
 
     while True:
@@ -15,5 +15,5 @@ def main():
         handle_client(conn, addr)
         conn.close()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
